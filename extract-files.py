0d8c7ed34dadd643d41617e7c8a26de03bc96c53
zenin1504 <kayozzprojects@yandex.com>
2025-03-21 00:35:39 +0000

    duchamp: Drop engineering mode for now
    Add "hardware/dolby" also (will replace new engineering mode)
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- extract-files.py
+++ extract-files.py
@@ -21,6 +21,7 @@ namespace_imports = [
     'device/xiaomi/duchamp',
     'hardware/mediatek',
     'hardware/xiaomi',
+    'hardware/dolby',
 ]
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
